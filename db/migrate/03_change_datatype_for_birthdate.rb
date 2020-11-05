06e47aa2aef4bec556d9c2256d8e13c467d22045
jessi ortega <ortegabx05@gmail.com>
2020-11-05 16:05:24 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change_column
-    change_column(:artist,:birthdate, type)
+    change_column(:artist, :birthdate, :datetime)
     
   end
 end
